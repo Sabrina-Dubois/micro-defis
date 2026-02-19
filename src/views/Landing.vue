38cb1a44192c4ed6044208c3562589d29e64a4a4
Dubois Sabrina <dubois.sabrina84@gmail.com>
2026-02-19 01:24:35 +0100

    b fix bug css Landing

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -107,7 +107,7 @@ const categories = [
     width: 100%;
     max-width: 520px;
     box-sizing: border-box;
-    padding: 16px 0;
+    padding: 16px;
     min-height: 100vh;
     padding-bottom: 40px;
     margin: 0 auto;
