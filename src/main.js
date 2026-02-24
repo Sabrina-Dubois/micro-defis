e71ea5495ebe7bcb91846b95382107dcf9959d60
Dubois Sabrina <dubois.sabrina84@gmail.com>
2026-02-24 01:03:15 +0100

    correct  pwa.js

--- src/main.js
+++ src/main.js
@@ -12,7 +12,6 @@ import "./assets/main.css";
 import { createI18n } from "vue-i18n";
 import en from "@/i18n/en.json";
 import fr from "@/i18n/fr.json";
-import { registerServiceWorker } from "./pwa";
 
 const i18n = createI18n({
   legacy: false,
